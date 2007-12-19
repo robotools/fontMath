f36e6a512037afe335a20cdc63c1ad49eaa19335
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2007-12-19 16:09:00 +0000

    Added test cases.

--- Lib/fontMath/mathKerning.py
+++ Lib/fontMath/mathKerning.py
@@ -68,6 +68,29 @@ class MathKerning(object):
         return g
 
     def __getitem__(self, pair):
+        """
+        >>> kerning = {
+        ...     ("@A_left", "@A_right") : 1,
+        ...     ("A1", "@A_right") : 2,
+        ...     ("@A_left", "A2") : 3,
+        ...     ("A3", "A3") : 4,
+        ... }
+        >>> groups = {
+        ... "@A_left" : ["A", "A1", "A2", "A3"],
+        ... "@A_right" : ["A", "A1", "A2", "A3"],
+        ... }
+        >>> obj = MathKerning(kerning, groups)
+        >>> obj["A", "A"]
+        1
+        >>> obj["A1", "A"]
+        2
+        >>> obj["A", "A2"]
+        3
+        >>> obj["A3", "A3"]
+        4
+        >>> obj["X", "X"]
+        0
+        """
         if self._kerning.has_key(pair):
             return self._kerning[pair]
         #
@@ -106,6 +129,29 @@ class MathKerning(object):
         return 0
 
     def guessPairType(self, pair):
+        """
+        >>> kerning = {
+        ...     ("@A_left", "@A_right") : 1,
+        ...     ("A1", "@A_right") : 2,
+        ...     ("@A_left", "A2") : 3,
+        ...     ("A3", "A3") : 4,
+        ... }
+        >>> groups = {
+        ... "@A_left" : ["A", "A1", "A2", "A3"],
+        ... "@A_right" : ["A", "A1", "A2", "A3"],
+        ... }
+        >>> obj = MathKerning(kerning, groups)
+        >>> obj.guessPairType(("@A_left", "@A_right"))
+        ('class', 'class')
+        >>> obj.guessPairType(("A1", "@A_right"))
+        ('exception', 'class')
+        >>> obj.guessPairType(("@A_left", "A2"))
+        ('class', 'exception')
+        >>> obj.guessPairType(("A3", "A3"))
+        ('exception', 'exception')
+        >>> obj.guessPairType(("A", "A"))
+        ('single', 'single')
+        """
         left, right = pair
         CLASS_TYPE = "class"
         SINGLE_TYPE = "single"
@@ -190,41 +236,186 @@ class MathKerning(object):
         return ks
 
     def __add__(self, other):
+        """
+        >>> kerning1 = {
+        ...     ("A", "A") : 1,
+        ...     ("B", "B") : 1,
+        ...     ("NotIn2", "NotIn2") : 1,
+        ...     ("@NotIn2", "C") : 1,
+        ...     ("@D", "@D") : 1,
+        ... }
+        >>> groups1 = {
+        ...     "@NotIn1" : ["C"],
+        ...     "@D" : ["D", "H"],
+        ... }
+        >>> kerning2 = {
+        ...     ("A", "A") : -1,
+        ...     ("B", "B") : 1,
+        ...     ("NotIn1", "NotIn1") : 1,
+        ...     ("@NotIn1", "C") : 1,
+        ...     ("@D", "@D") : 1,
+        ... }
+        >>> groups2 = {
+        ...     "@NotIn2" : ["C"],
+        ...     "@D" : ["D"],
+        ... }
+        >>> obj = MathKerning(kerning1, groups1) + MathKerning(kerning2, groups2)
+        >>> sorted(obj.items())
+        [(('@D', '@D'), 2), (('@NotIn1', 'C'), 1), (('@NotIn2', 'C'), 1), (('B', 'B'), 2), (('NotIn1', 'NotIn1'), 1), (('NotIn2', 'NotIn2'), 1)]
+        >>> sorted(obj.groups()["@D"])
+        ['D', 'H']
+        """
         k = self._processMathOne(other, add)
         k.cleanup()
         return k
 
     def __sub__(self, other):
+        """
+        >>> kerning1 = {
+        ...     ("A", "A") : 1,
+        ...     ("B", "B") : 1,
+        ...     ("NotIn2", "NotIn2") : 1,
+        ...     ("@NotIn2", "C") : 1,
+        ...     ("@D", "@D") : 1,
+        ... }
+        >>> groups1 = {
+        ...     "@NotIn1" : ["C"],
+        ...     "@D" : ["D", "H"],
+        ... }
+        >>> kerning2 = {
+        ...     ("A", "A") : -1,
+        ...     ("B", "B") : 1,
+        ...     ("NotIn1", "NotIn1") : 1,
+        ...     ("@NotIn1", "C") : 1,
+        ...     ("@D", "@D") : 1,
+        ... }
+        >>> groups2 = {
+        ...     "@NotIn2" : ["C"],
+        ...     "@D" : ["D"],
+        ... }
+        >>> obj = MathKerning(kerning1, groups1) - MathKerning(kerning2, groups2)
+        >>> sorted(obj.items())
+        [(('@NotIn1', 'C'), -1), (('@NotIn2', 'C'), 1), (('A', 'A'), 2), (('NotIn1', 'NotIn1'), -1), (('NotIn2', 'NotIn2'), 1)]
+        >>> sorted(obj.groups()["@D"])
+        ['D', 'H']
+        """
         k = self._processMathOne(other, sub)
         k.cleanup()
         return k
 
     def __mul__(self, value):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 0,
+        ...     ("B", "B") : 1,
+        ...     ("C2", "@C") : 0,
+        ...     ("@C", "@C") : 2,
+        ... }
+        >>> groups = {
+        ...     "@C" : ["C1", "C2"],
+        ...     "@C" : ["C1", "C2"],
+        ... }
+        >>> obj = MathKerning(kerning, groups) * 2
+        >>> sorted(obj.items())
+        [(('@C', '@C'), 4), (('B', 'B'), 2), (('C2', '@C'), 0)]
+        """
         k = self._processMathTwo(value, mul)
         k.cleanup()
         return k
 
     def __rmul__(self, value):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 0,
+        ...     ("B", "B") : 1,
+        ...     ("C2", "@C") : 0,
+        ...     ("@C", "@C") : 2,
+        ... }
+        >>> groups = {
+        ...     "@C" : ["C1", "C2"],
+        ...     "@C" : ["C1", "C2"],
+        ... }
+        >>> obj = 2 * MathKerning(kerning, groups)
+        >>> sorted(obj.items())
+        [(('@C', '@C'), 4), (('B', 'B'), 2), (('C2', '@C'), 0)]
+        """
         k = self._processMathTwo(value, mul)
         k.cleanup()
         return k
 
     def __div__(self, value):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 0,
+        ...     ("B", "B") : 4,
+        ...     ("C2", "@C") : 0,
+        ...     ("@C", "@C") : 4,
+        ... }
+        >>> groups = {
+        ...     "@C" : ["C1", "C2"],
+        ...     "@C" : ["C1", "C2"],
+        ... }
+        >>> obj = MathKerning(kerning, groups) / 2
+        >>> sorted(obj.items())
+        [(('@C', '@C'), 4), (('B', 'B'), 2), (('C2', '@C'), 0)]
+        """
         k = self._processMathTwo(value, div)
         k.cleanup()
         return k
 
     def __rdiv__(self, value):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 0,
+        ...     ("B", "B") : 4,
+        ...     ("C2", "@C") : 0,
+        ...     ("@C", "@C") : 4,
+        ... }
+        >>> groups = {
+        ...     "@C" : ["C1", "C2"],
+        ...     "@C" : ["C1", "C2"],
+        ... }
+        >>> obj = 2 / MathKerning(kerning, groups)
+        >>> sorted(obj.items())
+        [(('@C', '@C'), 4), (('B', 'B'), 2), (('C2', '@C'), 0)]
+        """
         k = self._processMathTwo(value, div)
         k.cleanup()
         return k
 
     def round(self, multiple=1):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 2,
+        ...     ("B", "B") : 4,
+        ...     ("C", "C") : 7,
+        ...     ("D", "D") : 9,
+        ... }
+        >>> obj = MathKerning(kerning)
+        >>> obj.round(5)
+        >>> sorted(obj.items())
+        [(('A', 'A'), 0), (('B', 'B'), 5), (('C', 'C'), 5), (('D', 'D'), 10)]
+        """
         multiple = float(multiple)
         for k, v in self._kerning.items():
             self._kerning[k] = int(round(int(round(v / multiple)) * multiple))
 
     def cleanup(self):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 0,
+        ...     ("B", "B") : 1,
+        ...     ("C", "@C") : 0,
+        ...     ("@C", "@C") : 1,
+        ... }
+        >>> groups = {
+        ...     "@C" : ["C", "C1"]
+        ... }
+        >>> obj = MathKerning(kerning, groups)
+        >>> obj.cleanup()
+        >>> sorted(obj.items())
+        [(('@C', '@C'), 1), (('B', 'B'), 1), (('C', '@C'), 0)]
+        """
         for (left, right), v in self._kerning.items():
             if v == 0:
                 leftType, rightType = self.guessPairType((left, right))
@@ -232,6 +423,16 @@ class MathKerning(object):
                     del self._kerning[left, right]
 
     def addTo(self, value):
+        """
+        >>> kerning = {
+        ...     ("A", "A") : 1,
+        ...     ("B", "B") : -1,
+        ... }
+        >>> obj = MathKerning(kerning)
+        >>> obj.addTo(1)
+        >>> sorted(obj.items())
+        [(('A', 'A'), 2), (('B', 'B'), 0)]
+        """
         for k, v in self._kerning.items():
             self._kerning[k] = v + value
 
@@ -241,3 +442,6 @@ class MathKerning(object):
         font.groups.update(self.groups())
 
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
