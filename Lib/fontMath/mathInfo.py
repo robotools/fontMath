23d4edacffb98e4ede4adbab4bd96f5156463220
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2012-02-02 19:11:47 +0000

    Rearranging.

--- Lib/fontMath/mathInfo.py
+++ Lib/fontMath/mathInfo.py
@@ -1,5 +1,259 @@
 from mathFunctions import add, sub, mul, div
 
+
+class MathInfo(object):
+
+    def __init__(self, infoObject):
+        for attr in _infoAttrs.keys():
+            if hasattr(infoObject, attr):
+                setattr(self, attr, getattr(infoObject, attr))
+
+    # ----
+    # Copy
+    # ----
+
+    def copy(self):
+        copied = MathInfo(self)
+        return copied
+
+    # ----
+    # Math
+    # ----
+
+    # math with other info
+
+    def __add__(self, otherInfo):
+        """
+        >>> info1 = MathInfo(_TestInfoObject())
+        >>> info2 = MathInfo(_TestInfoObject())
+        >>> info3 = info1 + info2
+        >>> written = {}
+        >>> expected = {}
+        >>> for attr, value in _testData.items():
+        ...     written[attr] = getattr(info3, attr)
+        ...     if isinstance(value, list):
+        ...         expectedValue = [v + v for v in value]
+        ...     else:
+        ...         expectedValue = value + value
+        ...     expected[attr] = expectedValue
+        >>> sorted(expected) == sorted(written)
+        True
+        """
+        copiedInfo = self.copy()
+        self._processMathOne(copiedInfo, otherInfo, add)
+        return copiedInfo
+
+    def __sub__(self, otherInfo):
+        """
+        >>> info1 = MathInfo(_TestInfoObject())
+        >>> info2 = MathInfo(_TestInfoObject())
+        >>> info3 = info1 - info2
+        >>> written = {}
+        >>> expected = {}
+        >>> for attr, value in _testData.items():
+        ...     written[attr] = getattr(info3, attr)
+        ...     if isinstance(value, list):
+        ...         expectedValue = [v - v for v in value]
+        ...     else:
+        ...         expectedValue = value - value
+        ...     expected[attr] = expectedValue
+        >>> sorted(expected) == sorted(written)
+        True
+        """
+        copiedInfo = self.copy()
+        self._processMathOne(copiedInfo, otherInfo, sub)
+        return copiedInfo
+
+    def _processMathOne(self, copiedInfo, otherInfo, funct):
+        # used by: __add__, __sub__
+        for attr in _infoAttrs.keys():
+            a = None
+            b = None
+            v = None
+            if hasattr(copiedInfo, attr):
+                a = getattr(copiedInfo, attr)
+            if hasattr(otherInfo, attr):
+                b = getattr(otherInfo, attr)
+            if a is not None and b is not None:
+                if isinstance(a, (list, tuple)):
+                    v = self._processMathOneNumberList(a, b, funct)
+                else:
+                    v = self._processMathOneNumber(a, b, funct)
+            elif a is not None and b is None:
+                v = a
+            elif b is not None and a is None:
+                v = b
+            if v is not None:
+                setattr(copiedInfo, attr, v)
+        self._processPostscriptWeightName(copiedInfo)
+
+    def _processMathOneNumber(self, a, b, funct):
+        return funct(a, b)
+
+    def _processMathOneNumberList(self, a, b, funct):
+        if len(a) != len(b):
+            return None
+        v = []
+        for index, aItem in enumerate(a):
+            bItem = b[index]
+            v.append(funct(aItem, bItem))
+        return v
+
+    # math with factor
+
+    def __mul__(self, factor):
+        """
+        >>> info1 = MathInfo(_TestInfoObject())
+        >>> info2 = info1 * 2.5
+        >>> written = {}
+        >>> expected = {}
+        >>> for attr, value in _testData.items():
+        ...     written[attr] = getattr(info2, attr)
+        ...     if isinstance(value, list):
+        ...         expectedValue = [v * 2.5 for v in value]
+        ...     else:
+        ...         expectedValue = value * 2.5
+        ...     expected[attr] = expectedValue
+        >>> sorted(expected) == sorted(written)
+        True
+        """
+        if isinstance(factor, tuple):
+            factor = factor[0]
+        copiedInfo = self.copy()
+        self._processMathTwo(copiedInfo, factor, mul)
+        return copiedInfo
+
+    __rmul__ = __mul__
+
+    def __div__(self, factor):
+        """
+        >>> info1 = MathInfo(_TestInfoObject())
+        >>> info2 = info1 / 2
+        >>> written = {}
+        >>> expected = {}
+        >>> for attr, value in _testData.items():
+        ...     written[attr] = getattr(info2, attr)
+        ...     if isinstance(value, list):
+        ...         expectedValue = [v / 2 for v in value]
+        ...     else:
+        ...         expectedValue = value / 2
+        ...     expected[attr] = expectedValue
+        >>> sorted(expected) == sorted(written)
+        True
+        """
+        if isinstance(factor, tuple):
+            factor = factor[0]
+        copiedInfo = self.copy()
+        self._processMathTwo(copiedInfo, factor, div)
+        return copiedInfo
+
+    __rdiv__ = __div__
+
+    def _processMathTwo(self, copiedInfo, factor, funct):
+        # used by: __mul__, __div__
+        for attr in _infoAttrs.keys():
+            if hasattr(copiedInfo, attr):
+                v = getattr(copiedInfo, attr)
+                if v is not None and factor is not None:
+                    if isinstance(v, (list, tuple)):
+                        v = self._processMathTwoNumberList(v, factor, funct)
+                    else:
+                        v = self._processMathTwoNumber(v, factor, funct)
+                else:
+                    v = None
+                setattr(copiedInfo, attr, v)
+        self._processPostscriptWeightName(copiedInfo)
+
+    def _processMathTwoNumber(self, v, factor, funct):
+        return funct(v, factor)
+
+    def _processMathTwoNumberList(self, v, factor, funct):
+        return [funct(i, factor) for i in v]
+
+    # special attributes
+
+    def _processPostscriptWeightName(self, copiedInfo):
+        """
+        >>> info = MathInfo(_TestInfoObject())
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Medium'
+        >>> info.openTypeOS2WeightClass = 549
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Medium'
+        >>> info.openTypeOS2WeightClass = 550
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Semi-bold'
+        >>> info.openTypeOS2WeightClass = 450
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Medium'
+        >>> info.openTypeOS2WeightClass = 449
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Normal'
+        >>> info.openTypeOS2WeightClass = 0
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Thin'
+        >>> info.openTypeOS2WeightClass = -1000
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Thin'
+        >>> info.openTypeOS2WeightClass = 1000
+        >>> info._processPostscriptWeightName(info)
+        >>> info.postscriptWeightName
+        'Black'
+        """
+        # handle postscriptWeightName by taking the value
+        # of openTypeOS2WeightClass and getting the closest
+        # value from the OS/2 specification.
+        name = None
+        if hasattr(copiedInfo, "openTypeOS2WeightClass") and copiedInfo.openTypeOS2WeightClass is not None:
+            v = copiedInfo.openTypeOS2WeightClass
+            v = int(round(v * .01) * 100)
+            if v < 100:
+                v = 100
+            elif v > 900:
+                v = 900
+            name = _postscriptWeightNameOptions[v]
+        copiedInfo.postscriptWeightName = name
+
+    # ----------
+    # Extraction
+    # ----------
+
+    def extractInfo(self, otherInfoObject):
+        """
+        >>> info1 = MathInfo(_TestInfoObject())
+        >>> info2 = info1 * 2.5
+        >>> info3 = _TestInfoObject()
+        >>> info2.extractInfo(info3)
+        >>> written = {}
+        >>> expected = {}
+        >>> for attr, value in _testData.items():
+        ...     written[attr] = getattr(info2, attr)
+        ...     if isinstance(value, list):
+        ...         expectedValue = [int(round(v * 2.5)) for v in value]
+        ...     elif isinstance(value, int):
+        ...         expectedValue = int(round(value * 2.5))
+        ...     else:
+        ...         expectedValue = value * 2.5
+        ...     expected[attr] = expectedValue
+        >>> sorted(expected) == sorted(written)
+        True
+        """
+        for attr, formatter in _infoAttrs.items() + [("postscriptWeightName", None)]:
+            if hasattr(self, attr):
+                v = getattr(self, attr)
+                if v is not None:
+                    if formatter is not None:
+                        v = formatter(v)
+                setattr(otherInfoObject, attr, v)
+
+
 # ----------
 # Formatters
 # ----------
@@ -113,248 +367,9 @@ _postscriptWeightNameOptions = {
     900 : "Black"
 }
 
-# ------
-# Object
-# ------
-
-class MathInfo(object):
-
-    def __init__(self, infoObject):
-        for attr in _infoAttrs.keys():
-            if hasattr(infoObject, attr):
-                setattr(self, attr, getattr(infoObject, attr))
-
-    def _processMathOne(self, copiedInfo, otherInfo, funct):
-        # used by: __add__, __sub__
-        for attr in _infoAttrs.keys():
-            a = None
-            b = None
-            v = None
-            if hasattr(copiedInfo, attr):
-                a = getattr(copiedInfo, attr)
-            if hasattr(otherInfo, attr):
-                b = getattr(otherInfo, attr)
-            if a is not None and b is not None:
-                if isinstance(a, (list, tuple)):
-                    v = self._processMathOneNumberList(a, b, funct)
-                else:
-                    v = self._processMathOneNumber(a, b, funct)
-            elif a is not None and b is None:
-                v = a
-            elif b is not None and a is None:
-                v = b
-            if v is not None:
-                setattr(copiedInfo, attr, v)
-        self._processPostscriptWeightName(copiedInfo)
-
-    def _processMathTwo(self, copiedInfo, factor, funct):
-        # used by: __mul__, __div__
-        for attr in _infoAttrs.keys():
-            if hasattr(copiedInfo, attr):
-                v = getattr(copiedInfo, attr)
-                if v is not None and factor is not None:
-                    if isinstance(v, (list, tuple)):
-                        v = self._processMathTwoNumberList(v, factor, funct)
-                    else:
-                        v = self._processMathTwoNumber(v, factor, funct)
-                else:
-                    v = None
-                setattr(copiedInfo, attr, v)
-        self._processPostscriptWeightName(copiedInfo)
-
-    def _processMathOneNumber(self, a, b, funct):
-        return funct(a, b)
-
-    def _processMathTwoNumber(self, v, factor, funct):
-        return funct(v, factor)
-
-    def _processMathOneNumberList(self, a, b, funct):
-        if len(a) != len(b):
-            return None
-        v = []
-        for index, aItem in enumerate(a):
-            bItem = b[index]
-            v.append(funct(aItem, bItem))
-        return v
-
-    def _processMathTwoNumberList(self, v, factor, funct):
-        return [funct(i, factor) for i in v]
-
-    def _processPostscriptWeightName(self, copiedInfo):
-        """
-        >>> info = MathInfo(_TestInfoObject())
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Medium'
-        >>> info.openTypeOS2WeightClass = 549
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Medium'
-        >>> info.openTypeOS2WeightClass = 550
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Semi-bold'
-        >>> info.openTypeOS2WeightClass = 450
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Medium'
-        >>> info.openTypeOS2WeightClass = 449
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Normal'
-        >>> info.openTypeOS2WeightClass = 0
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Thin'
-        >>> info.openTypeOS2WeightClass = -1000
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Thin'
-        >>> info.openTypeOS2WeightClass = 1000
-        >>> info._processPostscriptWeightName(info)
-        >>> info.postscriptWeightName
-        'Black'
-        """
-        # handle postscriptWeightName by taking the value
-        # of openTypeOS2WeightClass and getting the closest
-        # value from the OS/2 specification.
-        name = None
-        if hasattr(copiedInfo, "openTypeOS2WeightClass") and copiedInfo.openTypeOS2WeightClass is not None:
-            v = copiedInfo.openTypeOS2WeightClass
-            v = int(round(v * .01) * 100)
-            if v < 100:
-                v = 100
-            elif v > 900:
-                v = 900
-            name = _postscriptWeightNameOptions[v]
-        copiedInfo.postscriptWeightName = name
-
-    def copy(self):
-        copied = MathInfo(self)
-        return copied
-
-    def __add__(self, otherInfo):
-        """
-        >>> info1 = MathInfo(_TestInfoObject())
-        >>> info2 = MathInfo(_TestInfoObject())
-        >>> info3 = info1 + info2
-        >>> written = {}
-        >>> expected = {}
-        >>> for attr, value in _testData.items():
-        ...     written[attr] = getattr(info3, attr)
-        ...     if isinstance(value, list):
-        ...         expectedValue = [v + v for v in value]
-        ...     else:
-        ...         expectedValue = value + value
-        ...     expected[attr] = expectedValue
-        >>> sorted(expected) == sorted(written)
-        True
-        """
-        copiedInfo = self.copy()
-        self._processMathOne(copiedInfo, otherInfo, add)
-        return copiedInfo
-
-    def __sub__(self, otherInfo):
-        """
-        >>> info1 = MathInfo(_TestInfoObject())
-        >>> info2 = MathInfo(_TestInfoObject())
-        >>> info3 = info1 - info2
-        >>> written = {}
-        >>> expected = {}
-        >>> for attr, value in _testData.items():
-        ...     written[attr] = getattr(info3, attr)
-        ...     if isinstance(value, list):
-        ...         expectedValue = [v - v for v in value]
-        ...     else:
-        ...         expectedValue = value - value
-        ...     expected[attr] = expectedValue
-        >>> sorted(expected) == sorted(written)
-        True
-        """
-        copiedInfo = self.copy()
-        self._processMathOne(copiedInfo, otherInfo, sub)
-        return copiedInfo
-
-    def __mul__(self, factor):
-        """
-        >>> info1 = MathInfo(_TestInfoObject())
-        >>> info2 = info1 * 2.5
-        >>> written = {}
-        >>> expected = {}
-        >>> for attr, value in _testData.items():
-        ...     written[attr] = getattr(info2, attr)
-        ...     if isinstance(value, list):
-        ...         expectedValue = [v * 2.5 for v in value]
-        ...     else:
-        ...         expectedValue = value * 2.5
-        ...     expected[attr] = expectedValue
-        >>> sorted(expected) == sorted(written)
-        True
-        """
-        if isinstance(factor, tuple):
-            factor = factor[0]
-        copiedInfo = self.copy()
-        self._processMathTwo(copiedInfo, factor, mul)
-        return copiedInfo
-
-    __rmul__ = __mul__
-
-    def __div__(self, factor):
-        """
-        >>> info1 = MathInfo(_TestInfoObject())
-        >>> info2 = info1 / 2
-        >>> written = {}
-        >>> expected = {}
-        >>> for attr, value in _testData.items():
-        ...     written[attr] = getattr(info2, attr)
-        ...     if isinstance(value, list):
-        ...         expectedValue = [v / 2 for v in value]
-        ...     else:
-        ...         expectedValue = value / 2
-        ...     expected[attr] = expectedValue
-        >>> sorted(expected) == sorted(written)
-        True
-        """
-        if isinstance(factor, tuple):
-            factor = factor[0]
-        copiedInfo = self.copy()
-        self._processMathTwo(copiedInfo, factor, div)
-        return copiedInfo
-
-    __rdiv__ = __div__
-
-    def extractInfo(self, otherInfoObject):
-        """
-        >>> info1 = MathInfo(_TestInfoObject())
-        >>> info2 = info1 * 2.5
-        >>> info3 = _TestInfoObject()
-        >>> info2.extractInfo(info3)
-        >>> written = {}
-        >>> expected = {}
-        >>> for attr, value in _testData.items():
-        ...     written[attr] = getattr(info2, attr)
-        ...     if isinstance(value, list):
-        ...         expectedValue = [int(round(v * 2.5)) for v in value]
-        ...     elif isinstance(value, int):
-        ...         expectedValue = int(round(value * 2.5))
-        ...     else:
-        ...         expectedValue = value * 2.5
-        ...     expected[attr] = expectedValue
-        >>> sorted(expected) == sorted(written)
-        True
-        """
-        for attr, formatter in _infoAttrs.items() + [("postscriptWeightName", None)]:
-            if hasattr(self, attr):
-                v = getattr(self, attr)
-                if v is not None:
-                    if formatter is not None:
-                        v = formatter(v)
-                setattr(otherInfoObject, attr, v)
-
-
-# ------------
-# Test Support
-# ------------
+# ----
+# Test
+# ----
 
 _testData = dict(
     # generic
