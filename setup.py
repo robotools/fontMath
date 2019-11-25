519da52b9096377d4f02e9743a572b045e109110
Cosimo Lupo <clupo@google.com>
2019-11-25 13:32:56 +0000

    Bump version: 0.5.1 → 0.5.2.dev0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ wheel = ['wheel'] if needs_wheel else []
 
 setup(
     name="fontMath",
-    version="0.5.1",
+    version="0.5.2.dev0",
     description="A set of objects for performing math operations on font data.",
     # long_description=long_description,
     author="Tal Leming",
